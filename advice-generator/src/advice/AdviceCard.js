1c07e24161e71d3796b760a935cdd274e67e13eb
hayeon29 <hayoen209@gmail.com>
2023-10-10 20:32:12 +0900

    ✨ 버튼 클릭 시 API로부터 advice 받아오는 기능 추가
    주사위 문양이 그려진 버튼을 클릭했을 때 API로부터 무작위로 advice를 받아와 화면에 출력
    
    resolves: #3

--- advice-generator/src/advice/AdviceCard.js
+++ advice-generator/src/advice/AdviceCard.js
@@ -1,12 +1,31 @@
 import "./AdviceCard.css";
 
+import { useState } from "react";
+
 const AdviceCard = () => {
+    const [adviceNumber, setAdviceNumber] = useState(0);
+    const [adviceContent, setAdviceContent] = useState("");
+
+    const getAdvice = () => {
+        fetch("https://api.adviceslip.com/advice", {
+            headers: {
+                Accept: "application / json",
+            },
+            method: "GET",
+        })
+            .then((data) => data.json())
+            .then((data) => {
+                setAdviceNumber(data.slip.id);
+                setAdviceContent(data.slip.advice);
+            });
+    };
+
     return (
         <div className="advice-container">
-            <span className="advice-title">ADVICE #117</span>
-            <p className="advice-content">"It is easy to sit up and take notice, what's difficult is getting up and taking action."</p>
+            <span className="advice-title">{adviceNumber !== 0 && `ADVICE #${adviceNumber}`}</span>
+            <p className="advice-content">{adviceContent !== "" ? `"${adviceContent}"` : "Get Advice By Click Button!"}</p>
             <img className="advice-divider" src="/images/pattern-divider-desktop.svg" alt="pattern divider"></img>
-            <button className="advice-button get-new"></button>
+            <button className="advice-button get-new" onClick={getAdvice}></button>
         </div>
     );
 };
