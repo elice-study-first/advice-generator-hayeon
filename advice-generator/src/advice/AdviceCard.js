39f16838d39d0b09988a4b9db46185a373d136e3
hayeon29 <hayoen209@gmail.com>
2023-10-12 02:49:48 +0900

    ⚡️ React Query를 이용해 데이터 fetch
    컴포넌트 파일 내에서 fetch 함수를 사용해 데이터를 불러오기 ➡️ 다른 파일로 분리
    
    resolves: #11

--- advice-generator/src/advice/AdviceCard.js
+++ advice-generator/src/advice/AdviceCard.js
@@ -1,51 +1,44 @@
 import { LoadingSpinner } from "../loading/LoadingSpinner.js";
+import { fetchAdviceCard } from "../server/Fetcher.js";
 import "./AdviceCard.css";
 
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 const AdviceCard = () => {
     const [adviceNumber, setAdviceNumber] = useState(0);
     const [adviceContent, setAdviceContent] = useState("");
-    const [loading, setLoading] = useState(false);
-
-    const getAdvice = () => {
-        setLoading(true);
-        fetch("https://api.adviceslip.com/advice", {
-            headers: {
-                Accept: "application / json",
-            },
-            method: "GET",
-        })
-            .then((data) => data.json())
-            .then((data) => {
-                setAdviceNumber(data.slip.id);
-                setAdviceContent(data.slip.advice);
-                localStorage.setItem(
-                    "advice",
-                    JSON.stringify({
-                        number: data.slip.id,
-                        content: data.slip.advice,
-                    })
-                );
-                setLoading(false);
-            });
+    const { data, isFetching, refetch } = useQuery(["advice"], () => fetchAdviceCard(), {
+        enabled: false,
+    });
+    const getAdvice = async () => {
+        await refetch();
     };
 
     useEffect(() => {
+        if (data) {
+            localStorage.setItem(
+                "advice",
+                JSON.stringify({
+                    number: data.slip.id,
+                    content: data.slip.advice,
+                })
+            );
+        }
         const savedAdviceData = JSON.parse(localStorage.getItem("advice"));
         if (savedAdviceData) {
             setAdviceNumber(savedAdviceData.number);
             setAdviceContent(savedAdviceData.content);
         }
-    }, []);
+    }, [data]);
 
     return (
         <div className="advice-container">
-            {loading ? <LoadingSpinner></LoadingSpinner> : null}
+            {isFetching ? <LoadingSpinner></LoadingSpinner> : null}
             <span className="advice-title">{adviceNumber !== 0 && `ADVICE #${adviceNumber}`}</span>
             <p className="advice-content">{adviceContent !== "" ? `"${adviceContent}"` : "Get Advice By Click Button!"}</p>
             <img className="advice-divider" src="/images/pattern-divider-desktop.svg" alt="pattern divider"></img>
-            <button className="advice-button get-new" onClick={loading ? null : getAdvice} disabled={loading ? true : false}></button>
+            <button className="advice-button get-new" onClick={isFetching ? null : getAdvice} disabled={isFetching ? true : false}></button>
         </div>
     );
 };
